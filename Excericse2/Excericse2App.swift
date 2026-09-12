52804d98faad49389b4050d698e48ef254a2ef63
Emre Rauhofer <emre.rauhofer@gmail.com>
2026-09-12 17:09:20 +0200

    Initial

---
+++ Excericse2/Excericse2App.swift
@@ -0,0 +1,17 @@
+//
+//  Excericse2App.swift
+//  Excericse2
+//
+//  Created by emre on 12.09.26.
+//
+
+import SwiftUI
+
+@main
+struct Excericse2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
